3bfac742eaef6f33cde9febd3737a50bbdf38f81
Christian Paul Flores <florespaul161@gmail.com>
2024-01-04 07:52:37 +0800

    juan update icon

--- prebuilt_module/flutter_module/lib/main.dart
+++ prebuilt_module/flutter_module/lib/main.dart
@@ -84,6 +84,7 @@ class FullScreenView extends StatelessWidget {
       appBar: AppBar(
         title: const Text('New title'),
         title: const Text('Right title'),
+        icon: Icon(Icons.business),
       ),
       body: const Contents(showExit: true),
     );
